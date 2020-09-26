da30a2fa292559a37cb7217f516ada9a6a194fb8
Julie Sunny Mathew <42555719+Julie789@users.noreply.github.com>
2020-09-26 12:50:25 -0500

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,21 @@
+#Hangman.py
+#Name:
+#Date:
+
+#We will need time for this program, import to use this package.
+import time
+
+def main():
+  #welcoming the user
+
+  #here we set the guess word
+
+  #determine the number of turns
+  
+  #check if the turns are more than zero
+  
+  #see if the character is in the players guess
+  
+  # ask the user go guess a character
+
+main()
